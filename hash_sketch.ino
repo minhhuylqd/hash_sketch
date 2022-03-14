c9a9c7e6e2fc0c00276211a6a19b2450449c6266
minhhuylqd <minhhuy2810@gmail.com>
2022-03-14 23:11:30 +0200

    Edit style

--- hash_sketch.ino
+++ hash_sketch.ino
@@ -8,59 +8,59 @@ char receivedChars[numChars];
 boolean newData = false;
 
 void setup() {
-    Serial.begin(9600);
-    Serial.println("<Arduino is ready>");
+  Serial.begin(9600);
+  Serial.println("<Arduino is ready>");
 }
 
 void loop() {
-    recvWithEndMarker();
-    showNewData();
+  recvWithEndMarker();
+  showNewData();
 }
 
 //Note: Serial monitor has to set to Newline mode
 void recvWithEndMarker() {
-    static byte ndx = 0;
-    char endMarker = '\n';
-    char endStr = '\0';
-    char rc;
-    
-    while (Serial.available() > 0 && newData == false) {
-        rc = Serial.read();
+  static byte ndx = 0;
+  char endMarker = '\n';
+  char endStr = '\0';
+  char rc;
+  
+  while (Serial.available() > 0 && newData == false) {
+    rc = Serial.read();
 
-        if (rc != endMarker && rc != endStr) {
-            receivedChars[ndx] = rc;
-            ndx++;
-            if (ndx >= numChars) {
-                ndx = numChars - 1;
-            }
-        }
-        else {
-            receivedChars[ndx] = '\0'; // terminate the string
-            ndx = 0;
-            newData = true;
-        }
+    if (rc != endMarker && rc != endStr) {
+      receivedChars[ndx] = rc;
+      ndx++;
+      if (ndx >= numChars) {
+        ndx = numChars - 1;
+      }
     }
+    else {
+      receivedChars[ndx] = '\0'; // terminate the string
+      ndx = 0;
+      newData = true;
+    }
+  }
 }
 
 
 void showNewData() {
-    if (newData == true) {
-        Serial.print("Original String: ");
-        Serial.println(receivedChars);
+  if (newData == true) {
+    Serial.print("Original String: ");
+    Serial.println(receivedChars);
 
-        //MD5 hashed input string
-        unsigned char* hash = MD5::make_hash(receivedChars);
-        char* md5str = MD5::make_digest(hash, 16);
-        Serial.print("MD5 hashed: ");
-        Serial.println(md5str);
-        free(md5str);
-        free(hash);
+    //MD5 hashed input string
+    unsigned char* hash = MD5::make_hash(receivedChars);
+    char* md5str = MD5::make_digest(hash, 16);
+    Serial.print("MD5 hashed: ");
+    Serial.println(md5str);
+    free(md5str);
+    free(hash);
 
-        //SHA1 hashed input string
-        Serial.print("SHA1 hashed: ");
-        Serial.println(sha1(receivedChars));
-        
-        
-        newData = false;
-    }
+    //SHA1 hashed input string
+    Serial.print("SHA1 hashed: ");
+    Serial.println(sha1(receivedChars));
+    
+    
+    newData = false;
+  }
 }
